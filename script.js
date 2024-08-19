1d4b2f91f3e8e65beee8afb116c3cc841fe601ad
LogeshwariSenthilkumar <logeshwaris2003velur@gmail.com>
2024-08-19 21:41:21 +0530

    added calculator app

---
+++ script.js
@@ -0,0 +1,76 @@
+function byt7value()
+{
+   displays.value+=byt7.value;
+}
+function byt8value()
+{
+   displays.value+=byt8.value;
+}
+function byt9value()
+{
+   displays.value+=byt9.value;
+}
+function bytmvalue()
+{
+   displays.value+=bytm.value;
+}
+function byt4value()
+{
+   displays.value+=byt4.value;
+}
+function byt5value()
+{
+   displays.value+=byt5.value;
+}
+function byt6value()
+{
+   displays.value+=byt6.value;
+}
+function bytsvalue()
+{
+   displays.value+=byts.value;
+}
+function byt00value()
+{
+   displays.value+=byt00.value;
+}
+function byt0value()
+{
+   displays.value+=byt0.value;
+}
+function bytdotvalue()
+{
+   displays.value+=bytdot.value;
+}
+function bytdvalue()
+{
+   displays.value+=bytd.value;
+}
+function byt1value()
+{
+   displays.value+=byt1.value;
+}
+function byt2value()
+{
+   displays.value+=byt2.value;
+}
+function byt3value()
+{
+   displays.value+=byt3.value;
+}
+function bytavalue()
+{
+   displays.value+=byta.value;
+}
+function bytacvalue()
+{
+   displays.value="";
+}
+function bytdevalue()
+{
+   displays.value=displays.value.toString().slice(0,-1);
+}
+function bytevalue()
+{
+   displays.value=eval(displays.value);
+}
